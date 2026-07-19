723e053f7481e4c663c7ba0266bc8f5c8ee89e2e
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-19 16:13:00 +0530

    Refactor student management code by removing unused list implementation and consolidating student-related functionality

--- student.java
+++ student.java
@@ -1,139 +1,47 @@
 import java.util.*;
-class Node{
-	int data;
-	Node next;
-	Node(int data){this.data=data;}
+class Student{
+	private String id;	
+	private String name;	
+	private int prfMarks;
+	private int dbmsMarks;
+	
+	Student(){
+		
+	}
+	Student(String id, String name, int prfMarks, int dbmsMarks){
+		this.id=id;
+		this.name=name;
+		this.prfMarks=prfMarks;
+		this.dbmsMarks=dbmsMarks;
+	}
+	public String getId(){
+		return id;
+	}
+	public boolean equals(Object obj){
+		Student student=(Student)obj;
+		return id.equalsIgnoreCase(student.id);
+	}
+	public String toString(){
+		return "["+id+","+name+","+prfMarks+","+dbmsMarks+"]";
+	}
 }
-class List{
-	private Node first;
 
-	public void add(int data){ //Insertion order
-		addLast(data); //add(size(),data)
-	}
-	public void add(int index, int data){
-		if(index>=0 && index<=size()){
-			Node n1=new Node(data);
-			if(index==0){
-				n1.next=first;
-				first=n1;
-			}else{
-				int count=0;
-				Node temp=first;
-				while(count<index-1){
-					count++;
-					temp=temp.next;
-				}
-				n1.next=temp.next;
-				temp.next=n1;
-			}
-		}
-	}
-	public void addLast(int data){
-		add(size(),data);
-	}
-	public void addFirst(int data){
-		add(0,data);		
-	}
-	public void removeLast(){
-		remove(size()-1);
-	}
-	public void removeFirst(){
-		remove(0);
-	}
-	public void remove(int index){
-		if(!isEmpty() && index>=0 && index<size()){
-			if(index==0){
-				first=first.next;
-			}else{
-				int count=0;
-				Node temp=first;
-				while(count<index-1){
-					count++;
-					temp=temp.next;
-				}
-				temp.next=temp.next.next;
-			}
-		}
-	}
-	public int get(int index){
-		//
-		return -1;
-	}
-	public void printList(){
-		System.out.print("[");
-		Node temp=first;
-		while(temp!=null){
-			System.out.print(temp.data+", ");
-			temp=temp.next;
-		}
-		System.out.println(isEmpty() ?"empty]":"\b\b]");
-	}
-	public int size(){
-		Node temp=first;
-		int count=0;
-		while(temp!=null){
-			count++;
-			temp=temp.next;
-		}
-		return count;
-	}
-	public boolean isEmpty(){
-		return first==null;
-	}
-	public void clear(){
-		first=null;
-	}
-	public int search(int data){
-		Node temp=first;
-		int index=0;
-		while(temp!=null){
-			if(temp.data==data){
-				return index;
-			}
-			index++;
-			temp=temp.next;
-		}
-		return -1;
-	}
-	public int[] toArray(){
-		int[] tempDataArray=new int[size()];
-		Node temp=first;
-		for (int i = 0; i < tempDataArray.length; i++)	{
-			tempDataArray[i]=temp.data;
-			temp=temp.next;
-		}
-		return tempDataArray;
-	}
-}
 class Demo{
 	public static void main(String args[]){
-		List intList=new List();
-		intList.add(100);
-		intList.printList(); //[100]
+		ArrayList <Student>stList=new ArrayList<>();
+		stList.add(new Student("S0001","Nimal",65,67));
+		stList.add(new Student("S0002","Amal",85,70));
+		stList.add(new Student("S0003","Bimal",35,30));
+		stList.add(new Student("S0004","Ramal",55,70));
+		stList.add(new Student("S0005","Anil",95,90));
+		System.out.println(stList); 
 		
-		intList.clear();
-		intList.add(0,100);
-		intList.printList(); //[100]
+		for (int i = 0; i < stList.size(); i++){
+			Student s1=stList.get(i);
+			System.out.println(s1);
+		}
 		
-		intList.clear();
-		intList.addFirst(100);
-		intList.printList(); //[100]
-		
-		intList.clear();
-		intList.addLast(100);
-		intList.printList(); //[100]
-		
-		intList.remove(0);
-		intList.printList(); //[empty]
-		
-		intList.add(100);
-		intList.printList(); //[100]
-		intList.removeFirst();
-		intList.printList(); //[empty]
-		
-		intList.add(100);
-		intList.printList(); //[100]
-		intList.removeLast();
-		intList.printList(); //[empty]
+		stList.remove(new Student("S0003","Bimal",35,30));
+		System.out.println(stList); 
 	}
 }
